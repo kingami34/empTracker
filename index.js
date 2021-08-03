8db6367dc9a17705ca5c6b3b4ffa33820cb30542
Jeff Lynch <jlynch3@uw.edu>
2021-08-03 08:02:34 -0700

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const mysql = require('mysql');
 const inquirer = require('inquirer');
+
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 5001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -21,3 +15,20 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee database.`)
 );
+
+function choiceDepartment(){
+    inquirer.prompt([
+     {
+        type: "input",
+        name: "department",
+        message: "What would you like to do",
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department']
+     },  
+
+    ])
+    .then((answers) => {
+        console.info('Answer:', answers.department);
+        choiceDepartment()
+    })
+}
+
