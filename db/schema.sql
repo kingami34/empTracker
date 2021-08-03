169fbc6cb6411343b8a407d0d1e6921833bad182
Jeff Lynch <jlynch3@uw.edu>
2021-08-03 12:57:27 -0700

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -6,26 +6,27 @@ CREATE DATABASE employee;
 USE employee;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT,
+  id_department INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  FOREIGN KEY (id)
 );
 
-CREATE TABLE rrole (
-  id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE rolee (
+  id_role INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10,2) NOT NULL,
   department_id INT, 
-  PRIMARY KEY (id)
+  FOREIGN KEY (department_id)REFERENCES department(department_id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT,
+  id_employee INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR (30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT , 
   manager_id INT NOT NULL, 
-  PRIMARY KEY (id)
+  FOREIGN KEY (role_id)REFERENCES rolee(id_role) ON DELETE SET NULL
+  FOREIGN KEY (manager_id)REFERENCES manager(id_employee)
 );
 
 
