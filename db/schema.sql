31fe4d162283e4747bbd5b6b057c5ba39ace9c60
Jeff Lynch <jlynch3@uw.edu>
2021-08-04 10:49:12 -0700

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -5,27 +5,28 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+DROP DATABASE IF EXISTS department;
 CREATE TABLE department (
-  id_department: INT NOT NULL AUTO_INCREMENT,
-  name: VARCHAR(30) NOT NULL,
-  FOREIGN KEY (id)
+  id_department INT NOT NULL AUTO_INCREMENT,
+  names VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
-
+DROP DATABASE IF EXISTS roles;
 CREATE TABLE roles (  id_role: INT NOT NULL AUTO_INCREMENT,
-  title: VARCHAR(30) NOT NULL,
-  salary: DECIMAL(10,2) NOT NULL,
-  department_id: INT, 
-  FOREIGN KEY (department_id)REFERENCES department(department_id) ON DELETE CASCADE
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT, 
+  PRIMARY KEY (department_id)REFERENCES department(department_id) ON DELETE CASCADE
 );
-
+DROP DATABASE IF EXISTS employee;
 CREATE TABLE employee (
-  id_employee: INT NOT NULL AUTO_INCREMENT,
-  first_name: VARCHAR (30) NOT NULL,
-  last_name: VARCHAR(30) NOT NULL,
-  role_id: INT , 
-  manager_id: INT NOT NULL, 
-  FOREIGN KEY (role_id)REFERENCES rolee(id_role) ON DELETE SET NULL
-  FOREIGN KEY (manager_id)REFERENCES manager(manager_id)
+  id_employee INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR (30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT , 
+  manager_id INT NOT NULL, 
+  PRIMARY KEY (role_id)REFERENCES rolee(id_role) ON DELETE SET NULL
+  PRIMARY KEY (manager_id)REFERENCES manager(manager_id)
 );
 
 
