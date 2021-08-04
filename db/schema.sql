c7d42a53939093e7dddf63fa4393ea115c4a21fc
Jeff Lynch <jlynch3@uw.edu>
2021-08-04 12:42:47 -0700

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -5,23 +5,23 @@ DROP DATABASE IF EXISTS department;
 CREATE TABLE department (
   department_id INT NOT NULL AUTO_INCREMENT,
   names VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (department_id)
 );
 DROP DATABASE IF EXISTS roles;
 CREATE TABLE roles (
-  roles_id: INT NOT NULL AUTO_INCREMENT,
+  roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
   department_id INT,
-  FOREIGN KEY (department_id) REFERENCES department(id_department) ON DELETE CASCADE
+  FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
 DROP DATABASE IF EXISTS employee;
 CREATE TABLE employee (
-  employee_id INT NOT NULL AUTO_INCREMENT,
+  employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR (30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   roles_id INT,
   manager_id INT NOT NULL,
-  FOREIGN KEY (roles_id) REFERENCES roles(roles_id) ON DELETE 
-  FOREIGN KEY (manager_id) REFERENCES manager(manager_id)
+  FOREIGN KEY (roles_id) REFERENCES roles(roles_id) ON DELETE CASCADE, 
+  FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
