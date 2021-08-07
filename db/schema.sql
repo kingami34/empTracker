8e217f6912e70ee6c883e03c1ac045584323c0bc
Jeff Lynch <jlynch3@uw.edu>
2021-08-06 20:13:52 -0700

    first commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
-DROP DATABASE IF EXISTS department;
+
 CREATE TABLE department (
   department_id INT NOT NULL AUTO_INCREMENT,
   names VARCHAR(30) NOT NULL,
   PRIMARY KEY (department_id)
 );
-DROP DATABASE IF EXISTS roles;
+
 CREATE TABLE roles (
   roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -15,13 +15,13 @@ CREATE TABLE roles (
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(department_id) ON DELETE CASCADE
 );
-DROP DATABASE IF EXISTS employee;
+
 CREATE TABLE employee (
   employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
    first_name VARCHAR (30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   roles_id INT,
   manager_id INT,
-  FOREIGN KEY (roles_id) REFERENCES roles(roles_id) ON DELETE CASCADE, 
-  FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+ CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(roles_id) ON DELETE CASCADE, 
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,29 +1,30 @@
-USE employee_db
+USE employee_db;
 
-INSERT INTO departments (name)
+INSERT INTO department (names)
 VALUES ("Sales"),
     ("Engineering"),
-    ("Humn Resources"),
+    ("HumUn Resources"),
     ("Legal"),
     ("Finance"),
     ("Artist");
+    
 INSERT INTO roles (title, salary, department_id)
-VALUES ("CEO", "100000", "7"),
-    ("Software Developer", "70000", "2"),
-    ("Engineer", "60000", "3"),
-    ("Lawyer", "60000", "4"),
-    ("Actuary", "60000", "5"),
-    ("Artist", "70000", "6"),
-    ("Salesperson", "40000", "1");
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Bobby", "Knight", 1),
-    ("Bill", "Jones", 2, 1),
+VALUES ("CEO", "100000", 7),
+    ("Software Developer", "70000", 2),
+    ("Engineer", "60000", 3),
+    ("Lawyer", "60000", 4),
+    ("Actuary", "60000", 5),
+    ("Artist", "70000", 6),
+    ("Salesperson", "40000", 1);
+    
+    INSERT INTO employee (first_name, last_name, roles_id, manager_id)
+VALUES ("Bobby", "Knight", 1, NULL),
+	("Bill", "Jones", 2, 1),
     ("Mike", "Jones", 3, 1),
-    ("Charlie", "Brown", 4, 3),
+    ("Charlie", "Brown", 4, 1),
     ("David", "Banner", 5, 1),
     ("Scottie", "Pippen", 6, 1),
     ("Alvin", "Karama", 1, 1);
--- Query for view all --
 SELECT e.id,
     e.first_name,
     e.last_name,
